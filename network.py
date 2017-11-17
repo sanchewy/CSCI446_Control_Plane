cfb063134816238f54a80e242ff3f6ffc783dd11
sanchewy <sanchewy@gmail.com>
2017-11-16 23:02:50 -0700

    Changes to the assignment and refining existing methods.

--- network.py
+++ network.py
@@ -7,15 +7,13 @@ import queue
 import threading
 import re
 
-
 ## wrapper class for a queue of packets
 class Interface:
     ## @param maxsize - the maximum size of the queue storing packets
     #  @param cost - of the interface used in routing
-    def __init__(self, cost=0, maxsize=0):
+    def __init__(self, maxsize=0):
         self.in_queue = queue.Queue(maxsize);
         self.out_queue = queue.Queue(maxsize);
-        self.cost = cost
     
     ##get packet from the queue interface
     # @param in_or_out - use 'in' or 'out' interface
@@ -142,19 +140,19 @@ class Host:
 class Router:
     
     ##@param name: friendly router name for debugging
-    # @param intf_cost_L: outgoing cost of interfaces (and interface number) 
+    # @param num_intf: number of bidirectional interfaces
     # @param rt_tbl_D: routing table dictionary (starting reachability), eg. {1: {1: 1}} # packet to host 1 through interface 1 for cost 1
     # @param max_queue_size: max queue length (passed to Interface)
-    def __init__(self, name, intf_cost_L, rt_tbl_D, max_queue_size):
+    def __init__(self, name, num_intf, rt_tbl_D, max_queue_size):
         self.stop = False #for thread termination
         self.name = name
         #create a list of interfaces
-        #note the number of interfaces is set up by out_intf_cost_L
         self.intf_L = []
-        for cost in intf_cost_L:
-            self.intf_L.append(Interface(cost, max_queue_size))
+        for i in range(num_intf):
+            self.intf_L.append(Interface(max_queue_size))
         #set up the routing table for connected hosts
-        self.rt_tbl_D = rt_tbl_D 
+        self.rt_tbl_D = rt_tbl_D
+        self.rt_tbl_D.update({self.name:{0:0}})     #add self to routing table.
 
     ## called when printing the object
     def __str__(self):
@@ -193,25 +191,50 @@ class Router:
         
     ## forward the packet according to the routing table
     #  @param p Packet containing routing information
+    #  @param i Incoming interface number for packet p
     def update_routes(self, p, i):
-        #TODO: add logic to update the routing tables and
-        packet = RouteMessage.from_byte_S(p.to_byte_S(), p.to_byte_S())
-        routes = packet[2]
+        change_flag = False
+        packet = RouteMessage.from_byte_S(NetworkPacket.to_byte_S(p)[NetworkPacket.dst_addr_S_length+NetworkPacket.prot_S_length:])
+        print("Packet before: "+str(NetworkPacket.to_byte_S(p)))
         print('%s: Received routing update %s from interface %d' % (self, packet, i))
-        for route in routes:        #update the "i" neighbor
-            if route[0] == i:
-                if route[2] < self.rt_tbl_D[route[0]][route[1]]:
-                    self.rt_tbl_D[route[0]][route[1]] = route[2]
-        for route in routes:
-            if route[2] + self.rt_tbl_D[route[0]][i] < self.rt_tbl_D[route[0]][route[1]]:
-                self.rt_tbl_D[route[0]][route[1]] = route[2] + self.rt_tbl_D[route[0]][i]
+        sender_address = packet[0]
+        routes = packet[1]
+        print("sender address: "+str(sender_address))
+        print("self.rt: "+str(self.rt_tbl_D))
+        neighbor_path = list(self.rt_tbl_D[x] for x in self.rt_tbl_D if sender_address in self.rt_tbl_D.keys() and self.rt_tbl_D[sender_address] != [])
+      #  for x in self.rt_tbl_D:
+      #     if self.rt_tbl_D[x] == sender_address:
+      #          neighbor_path = x
+        first_route = None
+        if sender_address in routes.keys():
+            for intf in routes[sender_address].keys():
+                first_route = {sender_address:{intf:routes[sender_address][intf]}}
+        print("First Route:"+str(first_route)+" neighbor_path: "+str(neighbor_path))
+        if neighbor_path == [] or routes[first_route].itervalues()[0] < self.rt_tbl_D[neighbor_path].itervalues()[0]:   #If this node doesn't have a path to the sender node
+            self.rt_tbl_D.update({sender_address:{i:routes[first_route].itervalues()[0]}})
+            change_flag = True
+        for route in routes:        #update the "ith" path
+            existing_route = (x for x in self.rt_tbl_D if x[0] == route[0])
+            if not existing_route:
+                self.rt_tbl_D.update({route[0]:{i:(route[2]+neighbor_path[2])}})
+                change_flag = True
+            elif route[2] + neighbor_path[2] < existing_route[2]:
+                self.rt_tbl_D.update({route[0]:{i:(route[2]+neighbor_path[2])}})
+                change_flag = True
+            else:
+                pass
         # possibly send out routing updates
+        if change_flag:
+            for intf in self.num_intf:
+                self.send_routes(intf)
         
     ## send out route update
     # @param i Interface number on which to send out a routing update
     def send_routes(self, i):
         # a sample route update packet
-        p = RouteMessage(0, self.rt_tbl_D)
+        rm = RouteMessage(self.name, self.rt_tbl_D)
+        payload = rm.to_byte_S()
+        p = NetworkPacket(0, 'control', payload)
         try:
             #TODO: add logic to send out a route update
             print('%s: sending routing update "%s" from interface %d' % (self, p, i))
@@ -230,24 +253,29 @@ class Router:
         for key, value in self.rt_tbl_D.items():
             columns.insert(len(columns), key)
             for key2 in self.rt_tbl_D[key]:
-                rows.insert(len(rows), key2)        
-        print("     Cast to")
+                rows.insert(len(rows), key2)    
+        print("       Cost to")
         dest = "       "
         for i in columns:
             dest += (str(i))+" "
         print(dest)
         src = "From "
-        for i in rows:
-            src += str(i)
-            for column in self.rt_tbl_D:
-                if(str(self.rt_tbl_D[column][i]) == None):
-                    src += " ~"
-                else:
-                    src += " "+str(self.rt_tbl_D[column][i])
+        for i in range(len(rows)):
+            src += str(rows[i])
+            for j in range(len(columns)):
+                key1 = self.rt_tbl_D.get(columns[j])
+                if key1 is not None:
+                    key2 = key1.get(rows[i])
+                    if key2 is not None:
+                        src += " "+str(key2)
+                    else:
+                        src += " ~"
+                else: 
+                    src += " +"
             print(src)
-            src = ""
+            src = "     "
         print(self.rt_tbl_D)
-        print()
+        print()        
                 
     ## thread target for the host to keep forwarding data
     def run(self):
@@ -256,27 +284,24 @@ class Router:
             self.process_queues()
             if self.stop:
                 print (threading.currentThread().getName() + ': Ending')
-                return 
-
+                return                
+                
 class RouteMessage:
     ## packet encoding lengths 
-    dst_addr_S_length = 5
-    prot_S_length = 1
+    name_length = 5
     
     ##@param dst_addr: address of the destination host
     # @param data_S: the routing table from the router
     # @param prot_S: upper layer protocol for the packet (data, or control)
-    def __init__(self, dst_addr, data_S):
-        self.dst_addr = dst_addr
+    def __init__(self, name, data_S):
+        self.name = name
         self.data_S = data_S
 
     def __str__(self):
         return self.to_byte_S()
     
     def to_byte_S(self):
-        byte_S = str(self.dst_addr).zfill(self.dst_addr_S_length)
-        byte_S += '2'
-        
+        byte_S = str(self.name).zfill(self.name_length) 
         columns = list()
         rows = list()
         for key, value in self.data_S.items():
@@ -284,30 +309,22 @@ class RouteMessage:
             for key2 in self.data_S[key]:
                 rows.insert(len(rows), key2)
         routes = list()
-        for i in columns:
-            for j in rows:
-                routes.insert(len(routes), (i,j,self.data_S[i][j]))
+        for i in range(len(columns)):
+                dictionary = self.data_S[columns[i]][rows[i]]
+                routes.append((columns[i],rows[i],dictionary))
         byte_S += str(routes)
+        byte_S.replace("\'", "")
+        print("RouteMessage: "+byte_S)
         return byte_S
         
+    @classmethod
     def from_byte_S(self, byte_S):
-        dst_addr = int(byte_S[0 : RouteMessage.dst_addr_S_length])
-        prot_S = byte_S[RouteMessage.dst_addr_S_length : RouteMessage.dst_addr_S_length + RouteMessage.prot_S_length]
-        if prot_S == '1':
-            prot_S = 'data'
-        elif prot_S == '2':     #Should only be this one.
-            prot_S = 'control'
-        else:
-            raise('%s: unknown prot_S field: %s' %(self, prot_S))
-        data_S = byte_S[RouteMessage.dst_addr_S_length + RouteMessage.prot_S_length : ]
-        data_S = re.findall(r"[^[]*\[([^]]*)\]", data_S)
-        print("DATA_S: "+str(data_S))
+        name = byte_S[0 : RouteMessage.name_length].strip('0')
+        data_S = byte_S[RouteMessage.name_length : ]
+        data_S = re.findall(r"\(([^)]+)\)", data_S)
         new_dict = dict()
         for route in data_S:
-            divide = route.strip('()').split(",")
-            print("DIVIDE SIZE: "+str(len(divide))+" DIVIDE: "+str(divide))
+            divide = [x.strip(' ()\'') for x in route.split(",")]
             new_dict.update({divide[0]: {divide[1]: divide[2]}})
-        return dst_addr, prot_S, new_dict
-        
-        
-        
+        print("Name:"+str(name)+" New Dict: "+str(new_dict))
+        return name, new_dict
